99f2209d15ac8b833e1bd5630cb37d4d27a2dcb5
Zach Waugh <zwaugh@gmail.com>
2015-11-01 16:23:20 -0500

    Initial commit

---
+++ GIFKit/GIFKit.h
@@ -0,0 +1,19 @@
+//
+//  GIFKit.h
+//  GIFKit
+//
+//  Created by Zach Waugh on 11/1/15.
+//  Copyright © 2015 Zach Waugh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GIFKit.
+FOUNDATION_EXPORT double GIFKitVersionNumber;
+
+//! Project version string for GIFKit.
+FOUNDATION_EXPORT const unsigned char GIFKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIFKit/PublicHeader.h>
+
+
---
+++ GIFKitTests/GIFDecoderTests.swift
@@ -0,0 +1,13 @@
+//
+//  GIFDecoderTests.swift
+//  GIFKit
+//
+//  Created by Zach Waugh on 11/1/15.
+//  Copyright © 2015 Zach Waugh. All rights reserved.
+//
+
+import XCTest
+
+class GIFDecoderTests: XCTestCase {
+
+}
