70096df2cf82b39b80d81949a8caa1d19a104ce5
Zach Waugh <zwaugh@gmail.com>
2015-11-03 11:02:40 -0500

    Parse logical screen descriptor and start stubbing out parsing

--- GIFKitTests/GIFDecoderTests.swift
+++ GIFKitTests/GIFDecoderTests.swift
@@ -7,7 +7,50 @@
 //
 
 import XCTest
+import Quick
+import Nimble
+@testable import GIFKit
 
-class GIFDecoderTests: XCTestCase {
-
+class GIFDecoderSpec: QuickSpec {
+    override func spec() {
+//        describe(".hasValidSignature()") {
+//            context("when there is valid signature") {
+//                it("returns true") {
+//                    let data = "GIF89a".dataUsingEncoding(NSUTF8StringEncoding)!
+//                    let decoder = GIFDecoder(data: data)
+//                    expect(decoder.hasValidSignature()) == true
+//                }
+//            }
+//            
+//            context("when there is an invalid signature") {
+//                it("returns false") {
+//                    let data = "PNG89a".dataUsingEncoding(NSUTF8StringEncoding)!
+//                    let decoder = GIFDecoder(data: data)
+//                    expect(decoder.hasValidSignature()) == false
+//                }
+//            }
+//        }
+//        
+//        describe(".hasValidVersion()") {
+//            context("when version is valid") {
+//                it("returns true") {
+//                    var data = "GIF87a".dataUsingEncoding(NSUTF8StringEncoding)!
+//                    var decoder = GIFDecoder(data: data)
+//                    expect(decoder.hasValidVersion()) == true
+//                    
+//                    data = "GIF89a".dataUsingEncoding(NSUTF8StringEncoding)!
+//                    decoder = GIFDecoder(data: data)
+//                    expect(decoder.hasValidVersion()) == true
+//                }
+//            }
+//            
+//            context("when version is invalid") {
+//                it("returns false") {
+//                    let data = "GIF89b".dataUsingEncoding(NSUTF8StringEncoding)!
+//                    let decoder = GIFDecoder(data: data)
+//                    expect(decoder.hasValidVersion()) == false
+//                }
+//            }
+//        }
+    }
 }
---
+++ GIFKitTests/LogicalScreenDescriptorSpec.swift
@@ -0,0 +1,23 @@
+import XCTest
+import Quick
+import Nimble
+@testable import GIFKit
+
+class LogicalScreenDescriptorSpec: QuickSpec {
+    override func spec() {
+        it("parses descriptor from data") {
+            let bytes: [UInt8] = [0xC8, 0x00, 0xC8, 0x00, 0xA2, 0x00, 0x00]
+            let data = NSData(bytes: bytes, length: bytes.count)
+            
+            let logicalScreenDescriptor = LogicalScreenDescriptor(data: data)
+            expect(logicalScreenDescriptor.width) == 200
+            expect(logicalScreenDescriptor.height) == 200
+            expect(logicalScreenDescriptor.globalColorTableFlag) == true
+            expect(logicalScreenDescriptor.colorResolution) == 2
+            expect(logicalScreenDescriptor.sortFlag) == false
+            expect(logicalScreenDescriptor.globalColorTableSize) == 2
+            expect(logicalScreenDescriptor.backgroundColorIndex) == 0
+            expect(logicalScreenDescriptor.pixelAspectRatio) == 0
+        }
+    }
+}
